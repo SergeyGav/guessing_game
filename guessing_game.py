09eb3b538fd9764180101bd078f94b0632422d18
Sergey Gavrielov <sergey.gav82@gmail.com>
2025-12-17 12:47:05 +0200

    commited game file

--- guessing_game.py
+++ guessing_game.py
@@ -7,33 +7,51 @@ def is_valid_input(input_number):
     else:
         return False
 
-# def exit_game(input_letter):
-#
-#     if guessed_number.lower() == "exit":
-#         return  True
-#     return guessed_number
+def exit_game(input_number):
+    if input_number.lower() == "exit":
+        return True
+    else:
+        return False
+
 
 random_number = random.randrange(1,100)
-print(random_number)
+# print(random_number)
 number = input("Guess the secret number between 1-100: ")
-# result = exit_game(number)
+count = 0
 
+if exit_game(number) == False:
 
-while is_valid_input(number) == False:
-    number = input("Guess only numeric numbers between 1-100: ")
+    while is_valid_input(number) == False:
+        if exit_game(number) == True:
+            print("End of game")
+            exit()
+        number = input("Guess only numeric numbers between 1-100: ")
+        count = count + 1
 
-while int(number) != random_number:
+    while exit_game(number) != True and int(number) != random_number:
+        if int(number) > random_number:
+            print("The secret number is lower!")
+            count = count + 1
+            number = input("Guess the secret number again: ")
+            while is_valid_input(number) == False:
+                if exit_game(number) == True:
+                    print("End of game")
+                    exit()
+                count = count + 1
+                number = input("Guess only numeric numbers between 1-100: ")
 
-    if int(number) > random_number:
-        print("The secret number is lower!")
-        number = input("Guess the secret number again: ")
-        while is_valid_input(number) == False:
-            number = input("Guess only numeric numbers between 1-100: ")
+        else:
+            print("The secret number is higher!")
+            count = count + 1
+            number = input("Guess the secret number again: ")
+            while is_valid_input(number) == False:
+                if exit_game(number) == True:
+                    print("End of game")
+                    exit()
+                number = input("Guess only numeric numbers between 1-100: ")
+                count = count + 1
 
-    else:
-        print("The secret number is higher!")
-        number = input("Guess the secret number again: ")
-        while is_valid_input(number) == False:
-            number = input("Guess only numeric numbers between 1-100: ")
-
-print("You guessed right the secret number!")
+    print("You guessed right the secret number!")
+    print(f"It took to you {count} number of guesses")
+else:
+    print("End of game")
